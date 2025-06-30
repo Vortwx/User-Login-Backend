c3b3d44f476bd3c957583f0faebc8780f6b3746a
Vortwx <yiharn25@gmail.com>
2025-06-30 22:51:48 +0800

    Initialise backend environment - design user schema - setup SQlite as database

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    PrismaModule
+  ],
+  controllers: [],
+  providers: [PrismaService],
+})
+export class AppModule {}
